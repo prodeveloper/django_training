8302a867c23ac9ca97a27058ff17608ee0a9c8e9
Chencha Jacob <jmokua@gmail.com>
2015-09-30 11:03:23 +0300

    added first test model to the application

---
+++ home/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Student',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('full_name', models.CharField(max_length=200)),
+                ('age', models.IntegerField()),
+                ('email', models.EmailField(max_length=254)),
+                ('interests', models.TextField()),
+                ('registered_date', models.DateField(auto_now_add=True)),
+                ('last_update', models.DateField(auto_now=True)),
+            ],
+        ),
+    ]
--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    full_name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    email = models.EmailField()
+    interests = models.TextField()
+    registered_date = models.DateField(auto_now=False, auto_now_add=True)
+    last_update = models.DateField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.full_name
