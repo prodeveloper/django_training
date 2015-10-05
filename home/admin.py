0a64f692b710536b2f5c7c9ceb4bf1d3ff35a118
Chencha Jacob <jmokua@gmail.com>
2015-10-05 21:15:57 +0300

    enable posting to database

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Student
+from .forms import StudentForm
 # Register your models here.
 
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    form = StudentForm
+
+
+admin.site.register(Student, StudentAdmin)
--- home/views.py
+++ home/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .forms import StudentForm
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html' , {})
+    form = StudentForm(request.POST or None)
+
+    context = {
+        "hello_message": "Register new student",
+        "form": form
+    }
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        full_name = form.cleaned_data.get('full_name')
+        if full_name == "Jacob":
+            full_name = "Developer"
+        instance.full_name = full_name
+        instance.save()
+
+        context = {
+            "hello_message": "Student Saved"
+            }
+
+    return render(request, 'index.html', context)
